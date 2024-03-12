8dad7ff0612d2951082c2903beef4ac45bf8f91c
2-1-1-2 <tanyan.francois@gmail.com>
2024-03-12 14:06:17 +0100

    test build

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: 'https://2-1-1-2.github.io/',
+  plugins: [vue()],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue']
   }
 })

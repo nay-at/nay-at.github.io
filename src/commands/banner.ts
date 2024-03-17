fce3a36d082304dbc77291f0aea9d2ec3bac2d98
2-1-1-2 <tanyan.francois@gmail.com>
2024-03-17 16:34:38 +0100

    avancement css

--- src/commands/banner.ts
+++ src/commands/banner.ts
@@ -1,8 +1,8 @@
-import command from '../../config.json' assert {type: 'json'};
+import command from "../../config.json" assert { type: "json" };
 
-const createBanner = () : string[] => {
-  const banner : string[] = [];
-  banner.push("<br>")
+const createBanner = (): string[] => {
+  const banner: string[] = [];
+  banner.push("<br>");
   command.ascii.forEach((ele) => {
     let bannerString = "";
     //this is for the ascii art
@@ -13,16 +13,18 @@ const createBanner = () : string[] => {
         bannerString += ele[i];
       }
     }
-    
+
     let eleToPush = `<pre>${bannerString}</pre>`;
     banner.push(eleToPush);
-  });  
+  });
   banner.push("<br>");
-  banner.push("Welcome to WebShell v1.0.0");
-  banner.push("Type <span class='command'>'help'</span> for a list of all available commands.");
-  banner.push(`Type <span class='command'>'repo'</span> to view the GitHub repository or click <a href='${command.repoLink}' target='_blank'>here</a>.`);
+  banner.push("Welcome to 2-1-1-2's shell");
+  banner.push("Type or click <span class='command'>'help'</span> for a list of all available commands.");
+  /* banner.push(
+    `Type <span class='command'>'repo'</span> to view the GitHub repository or click <a href='${command.repoLink}' target='_blank'>here</a>.`
+  ); */
   banner.push("<br>");
   return banner;
-}
+};
 
 export const BANNER = createBanner();

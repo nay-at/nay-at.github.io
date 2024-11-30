84b1d1f5c7b8100b704882f1f9c2c15ab931de2f
2-1-1-2 <tanyan.francois@gmail.com>
2024-12-01 00:47:46 +0900

    internship

--- src/commands/whoami.ts
+++ src/commands/whoami.ts
@@ -2,7 +2,7 @@ const whoamiObj = {
   message: [
     [
       "I'm Tanya, a second-year student at a multimedia engineering school (IMAC).",
-      "I'm currently looking for a 3-month (12-week) internship starting May 6. ",
+      "I'm currently looking for a 6-month internship starting January 20 2025. ",
       "I'm mainly looking for work in the fields of video game programming and web development. ",
       "During my studies, I worked on a number of projects that gave me a wide range of skills",
       "that I'm sure I'll be able to put to good use during my internship. - ",

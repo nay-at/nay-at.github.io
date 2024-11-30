8c959bda2b6fc8ba710ed815e117437e68c24936
2-1-1-2 <tanyan.francois@gmail.com>
2024-12-01 00:37:13 +0900

    json setup + début script data

--- src/main.ts
+++ src/main.ts
@@ -1,404 +1,55 @@
-import command from "../config.json" assert { type: "json" };
-import { HELP } from "./commands/help";
-import { BANNER } from "./commands/banner";
-import { ABOUT } from "./commands/about";
-import { DEFAULT } from "./commands/default";
-import { PROJECTS } from "./commands/projects";
-import { createWhoami } from "./commands/whoami";
+$(document).ready(function () {
+  console.log("Hello World"); // Confirm script execution
+  let jsonSection: any[] = [];
+  let sections = ["whoami", "academics", "skills", "projects", "contact"];
 
-//mutWriteLines gets deleted and reassigned
-let mutWriteLines = document.getElementById("write-lines");
-let historyIdx = 0;
-let tempInput = "";
-let userInput: string;
-let isSudo = false;
-let isPasswordInput = false;
-let passwordCounter = 0;
-let bareMode = false;
+  for (let i = 0; i < sections.length; i++) {
+    console.log(`Processing section: ${sections[i]}`);
+    createSection(sections[i]);
+    console.log(`Section created: ${sections[i]}`);
 
-//WRITELINESCOPY is used to during the "clear" command
-const WRITELINESCOPY = mutWriteLines;
-const TERMINAL = document.getElementById("terminal");
-const USERINPUT = document.getElementById("user-input") as HTMLInputElement;
-const INPUT_HIDDEN = document.getElementById("input-hidden");
-const PASSWORD = document.getElementById("password-input");
-const PASSWORD_INPUT = document.getElementById("password-field") as HTMLInputElement;
-const PRE_HOST = document.getElementById("pre-host");
-const PRE_USER = document.getElementById("pre-user");
-const HOST = document.getElementById("host");
-const USER = document.getElementById("user");
-const PROMPT = document.getElementById("prompt");
-const COMMANDS = ["help", "about", "projects", "whoami", "banner", "credit", "clear"];
-const HISTORY: string[] = [];
-const SUDO_PASSWORD = command.password;
-const REPO_LINK = command.repoLink;
+    getDataHTML(sections[i]);
+    console.log(`HTML data loaded for: ${sections[i]}`);
 
-const scrollToBottom = () => {
-  const MAIN = document.getElementById("main");
-  if (!MAIN) return;
+    const jsonData = getDataJSON(sections[i]); // Wait for JSON data
+    console.log(`JSON data loaded for: ${sections[i]}`, jsonData);
 
-  MAIN.scrollTop = MAIN.scrollHeight;
-};
-
-function userInputHandler(e: KeyboardEvent) {
-  const key = e.key;
-
-  switch (key) {
-    case "Enter":
-      e.preventDefault();
-      if (!isPasswordInput) {
-        enterKey();
-      } else {
-        passwordHandler();
-      }
-
-      scrollToBottom();
-      break;
-    case "Escape":
-      USERINPUT.value = "";
-      break;
-    case "ArrowUp":
-      arrowKeys(key);
-      e.preventDefault();
-      break;
-    case "ArrowDown":
-      arrowKeys(key);
-      break;
-    case "Tab":
-      tabKey();
-      e.preventDefault();
-      break;
+    /* jsonSection.push(jsonData);
+    setDataDiv(jsonSection[i], sections[i]);
+    console.log(`Data set for: ${sections[i]}`); */
   }
+});
+
+// Function to create divs for the sections
+function createSection(name: string) {
+  console.log(`Creating section div for: ${name}`);
+  $("#content-container").append('<div id="' + name + '" class="content"></div>');
 }
 
-function enterKey() {
-  if (!mutWriteLines || !PROMPT) return;
-  const resetInput = "";
-  let newUserInput;
-  userInput = USERINPUT.value;
-
-  if (bareMode) {
-    newUserInput = userInput;
-  } else {
-    newUserInput = `<span class='output'>${userInput}</span>`;
-  }
-
-  HISTORY.push(userInput);
-  historyIdx = HISTORY.length;
-
-  //if clear then early return
-  if (userInput === "clear") {
-    commandHandler(userInput.toLowerCase().trim());
-    USERINPUT.value = resetInput;
-    userInput = resetInput;
-    return;
-  }
-
-  const div = document.createElement("div");
-  div.innerHTML = `<span id="prompt">${PROMPT.innerHTML}</span> ${newUserInput}`;
-
-  if (mutWriteLines.parentNode) {
-    mutWriteLines.parentNode.insertBefore(div, mutWriteLines);
-  }
-
-  /*
-  if input is empty or a collection of spaces, 
-  just insert a prompt before #write-lines
-  */
-  if (userInput.trim().length !== 0) {
-    commandHandler(userInput.toLowerCase().trim());
-  }
-
-  USERINPUT.value = resetInput;
-  userInput = resetInput;
-}
-
-function tabKey() {
-  let currInput = USERINPUT.value;
-
-  for (const ele of COMMANDS) {
-    if (ele.startsWith(currInput)) {
-      USERINPUT.value = ele;
-      return;
-    }
-  }
-}
-
-function arrowKeys(e: string) {
-  switch (e) {
-    case "ArrowDown":
-      if (historyIdx !== HISTORY.length) {
-        historyIdx += 1;
-        USERINPUT.value = HISTORY[historyIdx];
-        if (historyIdx === HISTORY.length) USERINPUT.value = tempInput;
-      }
-      break;
-    case "ArrowUp":
-      if (historyIdx === HISTORY.length) tempInput = USERINPUT.value;
-      if (historyIdx !== 0) {
-        historyIdx -= 1;
-        USERINPUT.value = HISTORY[historyIdx];
-      }
-      break;
-  }
-}
-
-function commandHandler(input: string) {
-  if (input.startsWith("rm -rf") && input.trim() !== "rm -rf") {
-    if (isSudo) {
-      if (input === "rm -rf src" && !bareMode) {
-        bareMode = true;
-
-        setTimeout(() => {
-          if (!TERMINAL || !WRITELINESCOPY) return;
-          TERMINAL.innerHTML = "";
-          TERMINAL.appendChild(WRITELINESCOPY);
-          mutWriteLines = WRITELINESCOPY;
-        });
-
-        easterEggStyles();
-        setTimeout(() => {
-          writeLines(["What made you think that was a good idea?", "<br>"]);
-        }, 200);
-
-        setTimeout(() => {
-          writeLines(["Now everything is ruined.", "<br>"]);
-        }, 1200);
-      } else if (input === "rm -rf src" && bareMode) {
-        writeLines(["there's no more src folder.", "<br>"]);
-      } else {
-        if (bareMode) {
-          writeLines(["What else are you trying to delete?", "<br>"]);
-        } else {
-          writeLines(["<br>", "Directory not found.", "type <span class='command'>'ls'</span> for a list of directories.", "<br>"]);
-        }
-      }
-    } else {
-      writeLines(["Permission not granted.", "<br>"]);
-    }
-    return;
-  }
-
-  switch (input) {
-    case "clear":
-      setTimeout(() => {
-        if (!TERMINAL || !WRITELINESCOPY) return;
-        TERMINAL.innerHTML = "";
-        TERMINAL.appendChild(WRITELINESCOPY);
-        mutWriteLines = WRITELINESCOPY;
-      });
-      break;
-    case "banner":
-      if (bareMode) {
-        writeLines(["WebShell v1.0.0", "<br>"]);
-        break;
-      }
-      writeLines(BANNER);
-      break;
-    case "help":
-      if (bareMode) {
-        writeLines(["maybe restarting your browser will fix this.", "<br>"]);
-        break;
-      }
-      writeLines(HELP);
-      break;
-    case "whoami":
-      if (bareMode) {
-        writeLines([`${command.username}`, "<br>"]);
-        break;
-      }
-      writeLines(createWhoami());
-      break;
-    case "about":
-      if (bareMode) {
-        writeLines(["Nothing to see here.", "<br>"]);
-        break;
-      }
-      writeLines(ABOUT);
-      break;
-    case "projects":
-      if (bareMode) {
-        writeLines(["I don't want you to break the other projects.", "<br>"]);
-        break;
-      }
-      writeLines(PROJECTS);
-      break;
-    case "credit":
-      writeLines(["Redirecting to github.com...", "<br>"]);
-      setTimeout(() => {
-        window.open(REPO_LINK, "_blank");
-      }, 500);
-      break;
-    case "linkedin":
-      //add stuff here
-      break;
-    case "github":
-      //add stuff here
-      break;
-    case "email":
-      //add stuff here
-      break;
-    case "rm -rf":
-      if (bareMode) {
-        writeLines(["don't try again.", "<br>"]);
-        break;
-      }
-
-      if (isSudo) {
-        writeLines(["Usage: <span class='command'>'rm -rf &lt;dir&gt;'</span>", "<br>"]);
-      } else {
-        writeLines(["Permission not granted.", "<br>"]);
-      }
-      break;
-    case "sudo":
-      if (bareMode) {
-        writeLines(["no.", "<br>"]);
-        break;
-      }
-      if (!PASSWORD) return;
-      isPasswordInput = true;
-      USERINPUT.disabled = true;
-
-      if (INPUT_HIDDEN) INPUT_HIDDEN.style.display = "none";
-      PASSWORD.style.display = "block";
-      setTimeout(() => {
-        PASSWORD_INPUT.focus();
-      }, 100);
-
-      break;
-    case "ls":
-      if (bareMode) {
-        writeLines(["", "<br>"]);
-        break;
-      }
-
-      if (isSudo) {
-        writeLines(["src", "<br>"]);
-      } else {
-        writeLines(["Permission not granted.", "<br>"]);
-      }
-      break;
-    default:
-      if (bareMode) {
-        writeLines(["type 'help'", "<br>"]);
-        break;
-      }
-
-      writeLines(DEFAULT);
-      break;
-  }
-}
-
-function writeLines(message: string[]) {
-  message.forEach((item, idx) => {
-    displayText(item, idx);
+//Function to get the data from the other HTML file
+function getDataHTML(name: string) {
+  $.get("./src/sections/" + name + ".html", function (data) {
+    console.log("./src/sections/" + name + ".html");
+    console.log("Data: " + data);
+    $("#" + name).html(data);
+    console.log("Data loaded: " + name);
   });
 }
 
-function displayText(item: string, idx: number) {
-  setTimeout(() => {
-    if (!mutWriteLines) return;
-    const p = document.createElement("p");
-    p.innerHTML = item;
-    mutWriteLines.parentNode!.insertBefore(p, mutWriteLines);
-    scrollToBottom();
-  }, 40 * idx);
-}
-
-function revertPasswordChanges() {
-  if (!INPUT_HIDDEN || !PASSWORD) return;
-  PASSWORD_INPUT.value = "";
-  USERINPUT.disabled = false;
-  INPUT_HIDDEN.style.display = "block";
-  PASSWORD.style.display = "none";
-  isPasswordInput = false;
-
-  setTimeout(() => {
-    USERINPUT.focus();
-  }, 200);
-}
-
-function passwordHandler() {
-  if (passwordCounter === 2) {
-    if (!INPUT_HIDDEN || !mutWriteLines || !PASSWORD) return;
-    writeLines(["<br>", "INCORRECT PASSWORD.", "PERMISSION NOT GRANTED.", "<br>"]);
-    revertPasswordChanges();
-    passwordCounter = 0;
-    return;
-  }
-
-  if (PASSWORD_INPUT.value === SUDO_PASSWORD) {
-    if (!mutWriteLines || !mutWriteLines.parentNode) return;
-    writeLines(["<br>", "PERMISSION GRANTED.", "Try <span class='command'>'rm -rf'</span>", "<br>"]);
-    revertPasswordChanges();
-    isSudo = true;
-    return;
-  } else {
-    PASSWORD_INPUT.value = "";
-    passwordCounter++;
-  }
-}
-
-function easterEggStyles() {
-  const bars = document.getElementById("bars");
-  const body = document.body;
-  const main = document.getElementById("main");
-  const span = document.getElementsByTagName("span");
-
-  if (!bars) return;
-  bars.innerHTML = "";
-  bars.remove();
-
-  if (main) main.style.border = "none";
-
-  body.style.backgroundColor = "black";
-  body.style.fontFamily = "VT323, monospace";
-  body.style.fontSize = "20px";
-  body.style.color = "white";
-
-  for (let i = 0; i < span.length; i++) {
-    span[i].style.color = "white";
-  }
-
-  USERINPUT.style.backgroundColor = "black";
-  USERINPUT.style.color = "white";
-  USERINPUT.style.fontFamily = "VT323, monospace";
-  USERINPUT.style.fontSize = "20px";
-  if (PROMPT) PROMPT.style.color = "white";
-}
-
-const initEventListeners = () => {
-  if (HOST) {
-    HOST.innerText = command.hostname;
-  }
-
-  if (USER) {
-    USER.innerText = command.username;
-  }
-
-  if (PRE_HOST) {
-    PRE_HOST.innerText = command.hostname;
-  }
-
-  if (PRE_USER) {
-    PRE_USER.innerText = command.username;
-  }
-
-  window.addEventListener("load", () => {
-    writeLines(BANNER);
+//function to get the data from a JSON file
+async function getDataJSON(name: string) {
+  return await $.getJSON("./src/json/" + name + ".json", function (data) {
+    console.log("Data loaded from ./src/json/" + name + ".json");
+    return data;
   });
+}
 
-  USERINPUT.addEventListener("keypress", userInputHandler);
-  USERINPUT.addEventListener("keydown", userInputHandler);
-  PASSWORD_INPUT.addEventListener("keypress", userInputHandler);
+// Function to add the content of the JSON into a div
+async function setDataDiv(data: any, name: string, content: string = "", lang: string = "en") {
+  console.log(`Setting data for: ${name}`, await data);
+  const sectionContent = (await data)[lang];
+  $("#" + name).html(sectionContent);
+}
 
-  window.addEventListener("click", () => {
-    USERINPUT.focus();
-  });
-
-  console.log(`%cPassword: ${command.password}`, "color: red; font-size: 20px;");
-};
-
-initEventListeners();
-
-export { commandHandler, userInputHandler, enterKey, COMMANDS, writeLines };
+// export the functions with jquery
+export { createSection, getDataJSON, setDataDiv };

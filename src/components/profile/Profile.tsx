980a51f0077c89abc6f286b0eef8ac32d3568bf3
2-1-1-2 <tanyan.francois@gmail.com>
2026-09-07 15:34:06 +0200

    wip

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -2,6 +2,11 @@ export function Profile() {
   return (
     <div>
       <h2>NAYAT</h2>
+      <div>
+        {/*       I'm Tanya, a second-year student at a multimedia engineering school (IMAC). I'm currently looking for a 6-month internship starting January 20
+        2025. I'm mainly looking for work in the fields of video game programming and web development. During my studies, I worked on a number of
+        projects that gave me a wide range of skills that I'm sure I'll be able to put to good use during my internship. - who am I? */}
+      </div>
       {/* <div>Tanya</div> */}
     </div>
   );
